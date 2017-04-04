5c84bc9313c133dfac16ae2e79c499e335508265
Алексей <alexfialko@hotmail.com>
2017-04-05 01:26:03 +0300

    add full navdrawer, update universityApi

--- app/src/main/java/heyalex/com/miet_schedule/ApplicationComponent.java
+++ app/src/main/java/heyalex/com/miet_schedule/ApplicationComponent.java
@@ -7,14 +7,20 @@ import android.content.Context;
 import javax.inject.Singleton;
 
 import dagger.Component;
+import heyalex.com.miet_schedule.api.UniversityApi;
 import heyalex.com.miet_schedule.navdrawer.NavDrawerModule;
+import heyalex.com.miet_schedule.navdrawer.NavDrawerPresenter;
 import heyalex.com.miet_schedule.navdrawer.NavDrawerPresenterImpl;
 
 @Singleton
 @Component(modules = {
         ApplicationModule.class,
-        NavDrawerModule.class})
+        NavDrawerModule.class
+})
 
 public interface ApplicationComponent {
-    ApplicationComponent plus(NavDrawerModule navDrawerModule);
+
+    //UniversityApi getUniversityApi();
+
+    NavDrawerPresenter getHeaderPresenter();
 }
