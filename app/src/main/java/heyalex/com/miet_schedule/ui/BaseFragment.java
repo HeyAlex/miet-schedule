abc4a7489a00660efb4a98ea6d8dfe6548929640
Алексей <alexfialko@hotmail.com>
2017-04-07 02:17:23 +0300

    update universityApi,  prepare schedule

---
+++ app/src/main/java/heyalex/com/miet_schedule/ui/BaseFragment.java
@@ -0,0 +1,80 @@
+package heyalex.com.miet_schedule.ui;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import butterknife.ButterKnife;
+
+/**
+ * Created by alexf on 08.09.2016.
+ */
+
+public abstract class BaseFragment extends Fragment {
+    public abstract void init();
+
+    public abstract int getResourceId();
+
+    public abstract void initView(View view);
+
+    //use for lazy load
+    protected abstract void onFragmentFirstVisible();
+
+    private boolean mIsFirstVisible = true;
+    private boolean mIsPrepared = false;
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        onPrepareStates();
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        init();
+    }
+
+    @Override
+    public void setUserVisibleHint(boolean isVisibleToUser) {
+        super.setUserVisibleHint(isVisibleToUser);
+        if (isVisibleToUser) {
+            onVisible();
+        } else {
+            onInvisible();
+        }
+    }
+
+
+    public void onVisible() {
+        if (mIsFirstVisible) {
+            mIsFirstVisible = false;
+            onPrepareStates();
+        }
+    }
+
+    public void onInvisible() {
+
+    }
+
+    private synchronized void onPrepareStates() {
+        if (mIsPrepared) {
+            onFragmentFirstVisible();
+        } else {
+            mIsPrepared = true;
+        }
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(getResourceId(), container, false);
+        ButterKnife.bind(this, view);
+        initView(view);
+        return view;
+    }
+
+}
