c5329412aa875b75d59d6fc21656774d3a7ce457
Алексей <alexfialko@hotmail.com>
2017-05-14 17:25:23 +0300

    add web view fragment

--- app/src/main/java/heyalex/com/miet_schedule/orioks/OrioksFragment.java
+++ app/src/main/java/heyalex/com/miet_schedule/orioks/OrioksFragment.java
@@ -1,8 +1,11 @@
 package heyalex.com.miet_schedule.orioks;
 
+import heyalex.com.miet_schedule.ui.BaseWebFragement;
+
 /**
  * Created by mac on 28.04.17.
  */
 
-public class OrioksFragment {
+public class OrioksFragment extends BaseWebFragement {
+
 }
