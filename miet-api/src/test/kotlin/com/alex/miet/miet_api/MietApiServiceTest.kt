d5e69d2f8a241df6c81b9090225955d353da5aa1
a.fialko <a.fialko@corp.mail.ru>
2020-05-31 15:54:21 +0300

    miet api dagger module

--- miet-api/src/test/kotlin/com/alex/miet/miet_api/MietApiServiceTest.kt
+++ miet-api/src/test/kotlin/com/alex/miet/miet_api/MietApiServiceTest.kt
@@ -2,16 +2,19 @@ package com.alex.miet.miet_api
 
 import com.alex.miet.miet_api.network.MietApiService
 import kotlinx.coroutines.runBlocking
+import okhttp3.OkHttpClient
 import org.junit.Test
 
 class MietApiServiceTest {
 
     @Test
     fun test() {
+        val client = OkHttpClient()
+        val api = MietApiService.create(client)
         runBlocking {
-            val groups = MietApiService.create().groups()
+            val groups = api.groups()
             println(groups)
-            println(MietApiService.create(). schedule(groups.first()))
+            println(api.schedule(groups.first()))
         }
     }
 }
