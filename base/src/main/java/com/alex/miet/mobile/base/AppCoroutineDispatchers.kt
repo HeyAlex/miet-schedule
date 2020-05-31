0684ae65c258a20cbfc7bb0890c26983e2f93d01
a.fialko <a.fialko@corp.mail.ru>
2020-05-31 15:53:20 +0300

    add base module

---
+++ base/src/main/java/com/alex/miet/mobile/base/AppCoroutineDispatchers.kt
@@ -0,0 +1,9 @@
+package com.alex.miet.mobile.base
+
+import kotlinx.coroutines.CoroutineDispatcher
+
+data class AppCoroutineDispatchers(
+    val io: CoroutineDispatcher,
+    val computation: CoroutineDispatcher,
+    val main: CoroutineDispatcher
+)
